2969a79749bfe8e584b463ebbd05c2cb6058e68c
Dr3xler <14806947+Dr3xler@users.noreply.github.com>
2021-05-03 14:48:50 +0200

    added cookies save functionality

--- website_check.py
+++ website_check.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil
 
 
 # TODO: Add ability to save cookies to both methods
@@ -20,7 +21,16 @@ driver.install_addon(extension_dir + extension, temporary=True)
 
 
 def load_with_addon(driver, websites):
-    """This method will load all websites with 'i don't care about cookies' preinstalled."""
+    """This method will load all websites with 'i don't care about cookies' preinstalled.
+    Afterwards it will convert the cookies to dicts and save them locally for comparison
+    Be aware that this method will delete all saved cookies"""
+
+    print('creating dir for cookies with addon...')
+
+    # checks if cookie dir already exists, creates an empty dir.
+    if len(os.listdir('data/save/with_addon/')) != 0:
+        shutil.rmtree('data/save/with_addon/')
+        os.mkdir('data/save/with_addon/')
 
     print('saving cookies in firefox with addons ...')
 
@@ -30,47 +40,65 @@ def load_with_addon(driver, websites):
     driver.install_addon(extension_dir + 'jid1-KKzOGWgsW3Ao4Q@jetpack.xpi', temporary=True)
 
     for website in websites:
-        name = website + ''
+        name = website.split('www.')[1]
         driver.get(website)
 
-        #TODO: list of dicts. check if dicts possible and how to json this.
         cookies_addons = driver.get_cookies()
         cookies_dict = {}
-        i = 0
         cookiecount = 0
 
-        #TODO: like this, it only saves the first value "title" and not the value inside "title". Add saving the information.
-        # needs something like for cookie in cookie_addons:
-        #                           for key, value in cookie:
-        #                               cookies_dict[key] = value
         for cookie in cookies_addons:
-            for value in cookie:
-                cookies_dict[i] = value
-                i += 1
+            cookies_dict = cookie
 
+            print('data/save/with_addon/%s/%s_%s.txt' % (name, name, cookiecount))
+            print(cookies_dict)
 
-
-            # cookies_dict[cookie['name']] = cookie['value']
-
-            with open('test.txt', 'w') as file:
+            # creates the website dir
+            if not os.path.exists('data/save/with_addon/%s/' % name):
+                os.mkdir('data/save/with_addon/%s/' % name)
+            # saves the cookies into the website dir
+            with open('data/save/with_addon/%s/%s_%s.txt' % (name, name, cookiecount), 'w') as file:
                 json.dump(cookies_dict, file)
-            cookiecount += 1
 
-        # with open('data/save/with_addon/' + website + '.json', 'w') as fp:
-        #    json.dump(cookies, fp, sort_keys=True, indent=4)
-        print(cookies_addons)
+            cookiecount += 1
 
 
 def load_without_addon(driver, websites):
-    """This method will load all websites on a vanilla firefox version"""
+    """This method will load all websites on a vanilla firefox version.
+    Afterwards it will convert the cookies to dicts and save them locally for comparison
+    Be aware that this method will delete all saved cookies"""
+
+    print('creating dir for cookies in vanilla...')
+
+    # checks if cookie dir already exists, creates an empty dir.
+    if len(os.listdir('data/save/without_addon/')) != 0:
+        shutil.rmtree('data/save/without_addon/')
+        os.mkdir('data/save/without_addon')
 
     print('saving cookies in firefox without addons ...')
 
     for website in websites:
+        name = website.split('www.')[1]
         driver.get(website)
-        cookies_vanilla = driver.get_cookies()
 
-        print(cookies_vanilla)
+        cookies_vanilla = driver.get_cookies()
+        cookies_dict = {}
+        cookiecount = 0
+
+        for cookie in cookies_vanilla:
+            cookies_dict = cookie
+
+            print('data/save/without_addon/%s/%s_%s.txt' % (name, name, cookiecount))
+            print(cookies_dict)
+
+            # creates the website dir
+            if not os.path.exists('data/save/without_addon/%s/' % name):
+                os.mkdir('data/save/without_addon/%s/' % name)
+            # saves the cookies into the website dir
+            with open('data/save/without_addon/%s/%s_%s.txt' % (name, name, cookiecount), 'w') as file:
+                json.dump(cookies_dict, file)
+
+            cookiecount += 1
 
 
 def close_driver_session(driver):
